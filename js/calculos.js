2dc4ebd81bd3f1fa3f135db66b4857acb0998173
RaylsonFernandes <Raylsonfernandes1994@gmail.com>
2020-05-06 12:15:49 -0300

    Inicio questão 2

--- js/calculos.js
+++ js/calculos.js
@@ -5,8 +5,7 @@ function mediaFinal(){
 	var nota1 = form_mediaFinal.nota1.value;
 	var nota2 = form_mediaFinal.nota2.value;
 
-	var soma = (nota1 + nota2) / 2;
-	
+	var soma = (parseFloat(nota1) + parseFloat(nota2)) / 2;
 
 	if (soma <= 3) {
 		var situacao = "Reprovado";
@@ -16,7 +15,39 @@ function mediaFinal(){
 		var situacao = "Aprovado";
 	}
 
-	document.getElementById("mediaFinal").innerHTML = nome + ", você está "
-		 + situacao + " na disciplina " + diciplina + 
-		 " com média " + soma;
+	$('#msgFinal').append(nome.toUpperCase() + ", você está "
+		 + situacao + " na disciplina " + diciplina.toUpperCase() + 
+		 " com média " + soma);
+
+}
+
+function carne(){
+	var nome = form_carne.nome.value;
+	var produto = form_carne.produto.value;
+	var preco = form_carne.preco.value;
+	var qtd = form_carne.qtd.value;
+	var parcela = form_carne.parcela.value;
+
+	preco = parseFloat(preco)	 * parseFloat(qtd);
+
+	if (parcela >= 3) {
+		var valorTotal = parseFloat(preco) + (parseFloat(preco) * 0.05);
+	}else if (parcela == 6) {
+		var valorTotal = parseFloat(preco) + (parseFloat(preco) * 0.12);
+	}else{
+		var valorTotal = preco;
+	}
+	var valorParcela = parseFloat(valorTotal) / parseInt(parcela);
+
+	$('#msgCarne').append(
+		"--------------------------------------------------------------------------------------------"+
+		"<br><b>LOJA DE ELETRODOMÉSTICOS</b><br>" + 
+		"<b>CLIENTE: </b>" + nome.toUpperCase() + 
+		"<br><b>PRODUTO: </b>" + produto.toUpperCase() + 
+		"<br><b>VALOR: </b>" + valorParcela + 
+		"<br>Parcela 1/" + parcela + "<br>"+ 
+		"--------------------------------------------------------------------------------------------"
+		);
+		
+	
 }
